ccd3bcb066c2416cda1b26ab95143050eb6bb9ed
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-28 01:03:12 +0530

    updation

---
+++ Todolist/Todolist/src/components/context.jsx
@@ -0,0 +1,10 @@
+export default function Content(){
+    return(
+        <>
+            <h1>TODO-LIST</h1>
+            <article>Input
+            <input type="text" placeholder="Enter Ur Task"></input>
+            </article>
+        </>
+    );
+}
