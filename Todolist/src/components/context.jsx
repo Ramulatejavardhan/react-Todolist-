8da2bd31392c1dfc9a80d1dc71d5e0c8631448d0
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-08-31 00:04:51 +0530

    usestate(onchange)

--- Todolist/src/components/context.jsx
+++ Todolist/src/components/context.jsx
@@ -4,7 +4,7 @@ export default function Content(){
     return(
         <>
             <h1>TODO LIST</h1>
-            <input type="text" placeholder="Enter Ur Task" onChange={}></input>
+            <input type="text" onChange={(event)=>settask(event.target.value)}  placeholder="Enter Ur Task"></input>
             <br></br>
             <button>ADD</button>
             <br></br>
