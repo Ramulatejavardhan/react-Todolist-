2941dbf89893fc6d3212ec64a2b87b290c3bf08f
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-09-04 16:23:38 +0530

    usestate(onchange)

--- Todolist/src/components/context.jsx
+++ Todolist/src/components/context.jsx
@@ -8,7 +8,7 @@ export default function Content(){
             <input type="text" onChange={(event)=>settask(event.target.value)}  placeholder="Enter Ur Task"></input>
             <br></br>
             <button onClick={()=>{if(task===""){
-                    alert("Enter a task inside the input field");
+                    alert("Enter ur task inside the input field");
                 }else{
                     settodos([...todos,task]); settask("");
                 }
@@ -24,7 +24,7 @@ export default function Content(){
             {todos.map((todo)=>(
                 <>
                 <button onClick={() => {const newtodos=todos.filter((item) => item!==todo);
-                    
+                    settodos(newtodos);
                 }}>Delete</button>
                 </>
             )
