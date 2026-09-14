e3c1a471aaa271ca26d3fbf4092fa421a78380e6
Ramula.teja <ramulatejavardhan6@gmail.com>
2026-09-14 20:21:35 +0530

    usestate(onchange)

--- Todolist/src/components/context.jsx
+++ Todolist/src/components/context.jsx
@@ -26,7 +26,7 @@ export default function Content(){
                 {todos.map((todo) => (
             <li key={todo.text}>
                 {todo.text}
-                <button>Complete</button>
+                <button>Completed</button>
         <button onClick={() =>{
             const newtodos = todos.filter(
                 (item) => item.text !== todo.text
